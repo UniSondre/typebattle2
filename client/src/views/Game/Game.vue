e1ea922c8069e1104ec004df9b395733a823f538
Ruben Kobbeltvedt <ruben@unimicro.no>
2025-04-11 13:32:41 +0200

    pRøV iGjEn

--- client/src/views/Game/Game.vue
+++ client/src/views/Game/Game.vue
@@ -6,7 +6,7 @@
                 <GameOver v-if="state.completed.value" :state="state" />
                 <GamePlaying v-else :state="state" />
             </div>
-            <UButton class="bg-[#B82BFF] hover:bg-[#B82BFF] h-10 w-min" @click="reload">restart</UButton>
+            <UButton class="bg-[#B82BFF] hover:bg-[#B82BFF] h-10 w-[5.4rem]" @click="reload">prøv igjen</UButton>
         </div>
 	</div>
 </template>
